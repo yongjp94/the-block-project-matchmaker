78b4588a597483487b341cd046ace206e544a6b7
min <innoleek@gmail.com>
2017-04-11 21:02:37 -0700

    min

--- TheBlockProject/Controllers/Profile.cs
+++ TheBlockProject/Controllers/Profile.cs
@@ -1,30 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Collections.Generic;
 
 namespace TheBlockProject.Controllers
 {
-    public class HomeController : Controller
+
+    public class Profile
     {
-        public ActionResult Index()
+        public string Id { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool Gender { get; set; }
+        public int Age { get; set; }
+        public string PhoneNum { get; set; }
+        public string Address { get; set; }
+        public string Neighborhood { get; set; }
+        public string Race { get; set; }
+        public bool IsMarried { get; set; }
+        public string PrimaryLanguage { get; set; }
+        public string OtherLanguage { get; set; }
+        public List<Questionarie> Questionarie { get; set; }
+
+        public Profile()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
         }
     }
 }
--- TheBlockProject/Controllers/QuestionarieAnswer.cs
+++ TheBlockProject/Controllers/QuestionarieAnswer.cs
@@ -6,13 +6,11 @@ using System.Web.Mvc;
 
 namespace TheBlockProject.Controllers
 {
-    public class Questionarie
+    public class QuestionarieAnswer
     {
         public int Id { get; set; }
-        public string Question { get; set; }
         public string Answer { get; set; }
         public string QuestionType { get; set; }
-        public string SurveyType { get; set; }
         public bool IsRequired { get; set; }
 
     }
