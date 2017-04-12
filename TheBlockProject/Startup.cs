47e48b89d32c777b0a5f0cc800bbdf47c464bb9d
yongjp94 <yongjun.park.94@gmail.com>
2017-04-11 18:06:12 -0700

    first commit

---
+++ TheBlockProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheBlockProject.Startup))]
+namespace TheBlockProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
